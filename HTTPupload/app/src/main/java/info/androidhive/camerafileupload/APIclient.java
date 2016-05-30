6057aa20e7e6e0cc8ede5c5095ce265aa17a8af4
SeanAda <D062403@global.corp.sap>
2016-05-30 10:23:37 +0200

    initial commit

---
+++ HTTPupload/app/src/main/java/info/androidhive/camerafileupload/APIclient.java
@@ -0,0 +1,40 @@
+package info.androidhive.camerafileupload;
+
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.PersistentCookieStore;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by D062403 on 19.05.2016.
+ */
+public class APIclient {
+
+    private AsyncHttpClient client;
+    private PersistentCookieStore cookieStore;
+    private Context context;
+
+    public APIclient(Context context) {
+
+        this.context = context;
+
+        client = new AsyncHttpClient();
+
+        cookieStore = new PersistentCookieStore(context);
+        client.setCookieStore(cookieStore);
+    }
+
+    public void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return Config.API_URL + relativeUrl;
+    }
+}
---
+++ HTTPupload/app/src/main/java/info/androidhive/camerafileupload/Config.java
@@ -0,0 +1,14 @@
+package info.androidhive.camerafileupload;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://sas.systemgrid.de:8080/Unveiled-Server/UploadFile";
+
+	//Login url
+	public static final String API_URL = "http://sas.systemgrid.de/unveiled/ftp/api/";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "unveiled";
+
+	public static int USER_ID = -1;
+}
